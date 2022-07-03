ee38d7332960c5cc865150ce89e9e87f26cb0938
gamer5459 <rocker5459@yahoo.com>
2022-07-03 11:23:25 -0400

    Completed Shared ViewModels Across Fragments CodeLab Updated Dependencies Cleaned up Code

---
+++ app/src/main/java/com/example/cupcake/model/DataViewModel.kt
@@ -0,0 +1,77 @@
+package com.example.cupcake.model
+
+import android.icu.text.NumberFormat
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.*
+
+private const val PRICE_PER_CUPCAKE = 2.00
+private const val PRICE_FOR_SAME_DAY = 3.00
+
+class DataViewModel : ViewModel() {
+    init {
+        resetOrder()
+    }
+
+    private var _quantity = MutableLiveData<Int>()
+    val quantity: LiveData<Int> = _quantity
+
+    private var _flavor = MutableLiveData<String>()
+    val flavor: LiveData<String> = _flavor
+
+    private var _date = MutableLiveData<String>()
+    val date: LiveData<String> = _date
+
+    private var _price = MutableLiveData<Double>()
+    val price: LiveData<String> = Transformations.map(_price) {
+        NumberFormat.getCurrencyInstance().format(it)
+    }
+
+    val dateOptions = getPickupOptions()
+
+    fun setQuantity(orderQuantity: Int) {
+        _quantity.value = orderQuantity
+        updatePrice()
+    }
+
+    fun setFlavor(flavor: String) {
+        _flavor.value = flavor
+    }
+
+    fun setDate(pickupDate: String) {
+        _date.value = pickupDate
+        updatePrice()
+    }
+
+    fun hasNoFlavorSet(): Boolean {
+        return _flavor.value.isNullOrEmpty()
+    }
+
+    private fun getPickupOptions(): List<String> {
+        val options = mutableListOf<String>()
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+        val calendar = Calendar.getInstance()
+        repeat(4) {
+            options.add(formatter.format(calendar.time))
+            calendar.add(Calendar.DATE, 1)
+        }
+        return options
+    }
+
+    private fun resetOrder() {
+        _quantity.value = 0
+        _flavor.value = ""
+        _date.value = dateOptions[0]
+        _price.value = 0.0
+    }
+
+    private fun updatePrice() {
+        var calculatedPrice = (_quantity.value ?: 0) * PRICE_PER_CUPCAKE
+        if (dateOptions[0] == _date.value) calculatedPrice += PRICE_FOR_SAME_DAY
+        _price.value = calculatedPrice
+    }
+
+}
